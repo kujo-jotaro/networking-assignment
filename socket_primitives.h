c8f5e3699409fc8b99e475ff58189ce5be9513d2
Federico Lo Iacono <federico.loiacono@org.bairesdev.com>
2020-06-21 00:45:47 -0300

    Skimmed down. No more overcomplications.

--- socket_primitives.h
+++ socket_primitives.h
@@ -24,27 +24,13 @@ typedef struct {
     uint16_t portno;
 } Port_t;
 
-// Messages are the stream of bytes the client and server send to each other
-// via a communication channel
-typedef struct Message {
-    void *content;
-    int length;
-} * Message_t;
-
 typedef enum {
     LS_NOT_LISTENING,
     LS_LISTENING,
     LS_COUNT,
 } ListenStatus;
 
-typedef enum {
-    ST_SERVER,
-    ST_CLIENT,
-    ST_COUNT,
-} SocketType;
-
 typedef struct Socket {
-    SocketType socket_type;
     int file_descriptor;
     Address_t address;
     Port_t port;
@@ -52,39 +38,16 @@ typedef struct Socket {
     uint16_t connections_permitted;
 } * Socket;
 
-typedef struct comm_channel {
-    struct socket * server;
-    struct socket * client;
-    Message_t * messages;
-} * CommChannel;
-
-Message_t message_constructor(int length, const char * content);
-
-void message_destructor(Message_t msg);
-
-int message_send(int fd, const char * content);
+Socket get_peer_by_address(Address_t address);
 
 Socket socket(SocketType socket_type, char * sockets_dir);
 
-void bind(Socket sock, Address_t addr, Port_t port);
+int bind(Socket sock, Address_t addr, Port_t port);
 
-void listen(Socket sock, uint16_t connections_permitted);
+int listen(Socket sock, uint16_t connections_permitted);
 
-CommChannel accept(Socket listening_socket, Address_t address, char * clients_dir);
+int accept(Socket listening_socket, Address_t address, char * clients_dir);
 
-Socket get_peer_by_address(Address_t address);
+int connect(Address_t server_address);
 
-// construir mensaje y enviar con write
-void connect(Address_t server_address);
-
-// The client sends a signal (writes) through its channel telling it to close
-// Frees up the port
-// Destructs itself
-void client_close_connection();
-
-// The server catches the signal (reads) through the channel
-// Frees up the port
-// Destructs the client socket
-void server_close_connection();
-
-void close(Socket * socket);
+int close(Socket * socket);
